569dda11d3c68707486e47ec01a55282c56c6b6e
xny <103381606+xny@users.noreply.github.com>
2022-05-17 15:36:48 +0200

    Update core.py

--- core.py
+++ core.py
@@ -1,13 +1,16 @@
 import requests
 
+pass = "password"
+username = "username"
+uuid = requests.get('https://httpbin.org/uuid').json()['uuid']
+
+
 PROXY = {
-    
-      "http": f"http://{run}",
-      "https": f"http://{run}"
-  }
+      "http": f"http://{proxy}",
+      "https": f"http://{proxy}"
+}
 
 HEAD = {
-    
     'Host': 'i.instagram.com', 
     'Accept': '*/*', 
     'User-Agent': User_Agent(), 
@@ -17,19 +20,16 @@ HEAD = {
     'X-IG-Capabilities': '3brTvw==', 
     'X-IG-Connection-Type': 'WIFI',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    
-  }
+}
  
-  DATA = {
-    
+DATA = {
     'uuid': uuid.json()['uuid'], 
-    'password': pess, 
-    'username': self.user, 
-    'device_id': uuid.json()['uuid'], 
+    'password': pass, 
+    'username': username, 
+    'device_id': uuid, 
     'from_reg': 'false', 
     '_csrftoken': 'missing', 
     'login_attempt_countn': '0'
-  
-  }
+}
 
 brute = requests.post('https://i.instagram.com/api/v1/accounts/login/',headers=HEAD, data=DATA,proxies=PROXY, allow_redirects=True)
